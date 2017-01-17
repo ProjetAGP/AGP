bf5bcd17f02f24e8c5385af4e2b8abc5e202c5b1
Arthemis Whitecrow <bertie95@hotmail.fr>
2017-01-17 13:31:02 +0100

    Ajout de fonctions de Parsing et d'une fonction mathématique dans au package tools

--- AGP/src/tools/parse/StringParser.java
+++ AGP/src/tools/parse/StringParser.java
@@ -1,5 +1,10 @@
 package tools.parse;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class StringParser {
 
 	public static int getIndexOfWord(String w, String chain) {
@@ -15,5 +20,33 @@ public class StringParser {
 		}
 		return -1;
 	}
+	
+	public static String getNextWord(String line, int index,char separator){
+		/**get the whole text between index and the next separation*/
+		int last = index;
+		while(line.charAt(index) != separator)
+			index++;
+		String word = line.substring(last,index);
+		System.out.println(word);
+		return word;
+	}
+	
+	public static ArrayList<String> readData(String fileName) {
+		/**Read the file and place each lines of the text in an ArrayList<String>*/
+		ArrayList<String> lines = new ArrayList<String>();
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				lines.add(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lines;
+	}
+
 
 }
